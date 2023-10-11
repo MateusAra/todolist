2530d787680bf6f9eea3ae904816505851670c88
MateusAra <mateus19araujo@hotmail.com>
2023-10-10 22:20:27 -0300

    adicionando integração com banco de dados

--- src/main/java/br/com/mateusaraujo/todolist/user/UserController.java
+++ src/main/java/br/com/mateusaraujo/todolist/user/UserController.java
@@ -12,6 +12,6 @@ public class UserController
     @PostMapping("/createUser")
     public void createUser(@RequestBody UserModel user)
     {
-        System.out.println(user.name);
+        System.out.println(user.getName());
     }
 }
