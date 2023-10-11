ffa582ae233a0f3dda13fe45a3a4b8555139ff7d
MateusAra <mateus19araujo@hotmail.com>
2023-10-10 22:34:49 -0300

    criando metodo de add user

--- src/main/java/br/com/mateusaraujo/todolist/user/UserController.java
+++ src/main/java/br/com/mateusaraujo/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.mateusaraujo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController 
 {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/createUser")
-    public void createUser(@RequestBody UserModel user)
+    public UserModel createUser(@RequestBody UserModel user)
     {
-        System.out.println(user.getName());
+        var newUser = this.userRepository.save(user);
+        return newUser;
     }
 }
