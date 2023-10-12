0a92c361a15e9d99f63791ff7032fcbc17d56357
MateusAra <mateus19araujo@hotmail.com>
2023-10-12 11:15:14 -0300

    implementando camada de autenticação

--- src/main/java/br/com/mateusaraujo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/mateusaraujo/todolist/filter/FilterTaskAuth.java
@@ -1,8 +1,15 @@
 package br.com.mateusaraujo.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.mateusaraujo.todolist.user.IUserRepository;
+import br.com.mateusaraujo.todolist.user.UserModel;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,11 +17,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded); 
+        var auth = new String(authDecode);
+
+        String[] credentials = auth.split(":");
+        UserModel user = this.userRepository.findByUsername(credentials[0]);
+
+        if (user == null)
+        {
+            response.sendError(401);
+        }
+        else
+        {
+            var passwordVerify = BCrypt.verifyer().verify(credentials[1].toCharArray(), user.getPassword());
+
+            if(passwordVerify.verified)
+            {
+                filterChain.doFilter(request, response);
+            }
+            else
+            {
+                response.sendError(401);
+            }
+        }
     }
 }
